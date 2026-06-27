b1101a75c0e99191a163c324c9121c32cae123b1
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-27 12:35:03 +0530

    new update

--- Backend/src/router/auth.router.js
+++ Backend/src/router/auth.router.js
@@ -8,6 +8,7 @@ router.post("/login",userLogin)
 router.post("/update_profile_picture", upload.single("profilePicture"), uploadProfilePicture);
 router.route("/user_update").post(updateuserprofile);
 router.route("/get_user_update_profile").get(getuserupdateprofile);
+router.route("/update_Profile_Data").post(updateProfileData);
 
 
 export default router;
