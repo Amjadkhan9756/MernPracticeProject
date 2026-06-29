b7eb66644a77535d912f9fbeec215db45aacffca
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-29 09:41:14 +0530

    creating post

--- Backend/src/router/post.router.js
+++ Backend/src/router/post.router.js
@@ -1,8 +1,24 @@
 import {Router} from "express";
-
+import createPost from "../controller/post.controller.js";
+import multer from "multer"
 const router = Router();
 
+//multer profile picture here
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+ filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const ext = path.extname(file.originalname); // keep file extension
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+  }
+})
+
+const upload = multer({ storage })   
 
 
+
+router.post("/post",upload.single("videos"),createPost)
 
 export default router;
