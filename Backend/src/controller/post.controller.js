c43667f1fe87a9c4e5efff4940fc49249e7fb954
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-30 15:17:53 +0530

    new update

--- Backend/src/controller/post.controller.js
+++ Backend/src/controller/post.controller.js
@@ -11,3 +11,36 @@ export const activeCheck = async (req, res) => {
 
 }
 
+
+export const createPost = async (req,res)=>{
+    try{
+        const {token} = req.body;
+
+        const user = await User.findOne({ token });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const newPost = new Post ({
+            userId:user_id,
+            body:req.body.body,
+            media:req.file != undefined ? req.file.filename : " ",
+            filetype:req.file !=undefined ? req.file.mimetype.split("/")[1] : " "
+        });
+        await newPost.save();
+
+        return res.status(200).json({
+            message: "Post created successfully"
+        });
+
+        // Continue with post creation logic
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
+const getAllPost = async (req,res) =>{
+   
+}
--- Backend/src/router/auth.router.js
+++ Backend/src/router/auth.router.js
@@ -1,8 +1,22 @@
 import authRouter from "express"
 import {userRegister} from "../controller/User.controller.js"
 import { userLogin } from "../controller/User.controller.js";
+import multer from "multer";
 const router = authRouter();
 
+
+
+//multer profile picture here
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage })
 router.post("/register", userRegister);
 router.post("/login",userLogin)
 router.post("/update_profile_picture", upload.single("profilePicture"), uploadProfilePicture);
